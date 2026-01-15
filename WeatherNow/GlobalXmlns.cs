d5cab0f2e1c3a1d0d4130d6f8d7971f8af7b5ede
Enisx86 <enisselimovic2005@gmail.com>
2026-01-15 16:56:18 +0100

    Add project files.

---
+++ WeatherNow/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "WeatherNow")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "WeatherNow.Pages")]
