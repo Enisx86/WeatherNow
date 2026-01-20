0ea984614fc3751f8e52ae8cb33d98a50e51146c
Anel Baručija <anelkobarucija@outlook.com>
2026-01-20 19:51:03 +0100

    Added floating bottom navbar Added navbar functionality Added SearchPage

---
+++ WeatherNow/Controls/FloatingNavbar.xaml.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace WeatherNow.Controls;
+
+public partial class FloatingNavbar : ContentView
+{
+    public static readonly BindableProperty ActiveTabProperty =
+    BindableProperty.Create(nameof(ActiveTab), typeof(string), typeof(FloatingNavbar), "",
+    propertyChanged: (bindable, oldValue, newValue) => {
+        if (bindable is not FloatingNavbar navbar) return;
+        if (newValue is not string tab) return;
+
+        navbar.HomeButton.BackgroundColor = Colors.Transparent;
+        navbar.SearchButton.BackgroundColor = Colors.Transparent;
+
+        if (tab == "Home") navbar.HomeButton.BackgroundColor = Colors.LightBlue;
+        if (tab == "Search") navbar.SearchButton.BackgroundColor = Colors.LightBlue;
+    });
+
+    public string ActiveTab
+    {
+        get => (string)GetValue(ActiveTabProperty);
+        set => SetValue(ActiveTabProperty, value); 
+    }
+
+    public FloatingNavbar()
+	{
+        InitializeComponent();
+    }
+
+    private async void Home_Clicked(object sender, EventArgs e)
+    {
+		if (ActiveTab == "Home") return; // don't refresh
+		if (sender is not ImageButton button) return;
+
+		await Shell.Current.GoToAsync("//MainPage");
+    }
+
+    private async void Search_Clicked(object sender, EventArgs e)
+    {
+        if (ActiveTab == "Search") return; // don't refresh
+        if (sender is not ImageButton button) return;
+
+        await Shell.Current.GoToAsync("//SearchPage");
+    }
+}
